cab2a6172408f31be8ba3f2c340943de73f4908e
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-10-03 22:45:00 -0300

    remove useless stuff

--- app.js
+++ app.js
@@ -3,14 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -31,8 +26,9 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
+  let errBody = {error: err.message, status: err.status}
   res.status(err.status || 500);
-  res.render('error');
+  res.json(errBody)
 });
 
 module.exports = app;
