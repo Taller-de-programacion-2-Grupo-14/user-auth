abb8a0da5ab5a6dd3a5e90373a145947a6168fb8
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-10-08 18:35:21 -0300

    Still trying to make heroku work

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
