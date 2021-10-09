b44929b0099f57bdfcf3f8ad9d3237cdefa43d02
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-10-09 14:05:34 -0300

    Fix broken test and do check of workflow

--- controllers/users.test.js
+++ controllers/users.test.js
@@ -4,11 +4,11 @@ describe("controller.js tests", ()=> {
     beforeAll(() => {
         mockService.AddUser = jest.fn()
     })
-    test("controller won't throw error if service okay", () => {
+    test("controller won't throw error if service okay", async () => {
         mockService.AddUser.mockReturnValueOnce("coso")
         let res = {json: jest.fn(), status: jest.fn()}
         let controller = new Users(mockService);
-        controller.HandleUserPost({body: {}}, res)
+        await controller.HandleUserPost({body: {}}, res)
         expect(mockService.AddUser.mock.calls.length).toBe(1)
         expect(res.json.mock.calls.length).toBe(1)
         expect(res.status.mock.calls[0][0]).toBe(200)
