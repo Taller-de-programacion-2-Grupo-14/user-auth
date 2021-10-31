29c1d4c6e86591f055418f7e4c0ddc9a45a3abc5
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-10-31 03:20:55 -0300

    Add code to check for when should login or register and login

---
+++ controllers/firebase.js
@@ -0,0 +1,29 @@
+class Firebase {
+    constructor(db) {
+        this.db = db;
+    }
+
+    async ProcessFirebaseInfoNecessary(req) {
+        let fullName = req.body.displayName.split(' ');
+        const name = fullName.shift();
+        const lastName = fullName.join(" ");
+        const email = req.body.email;
+        const password = fullName.join('') + email
+        let values = {
+            email: req.body.email,
+            photoURL: req.body.photoURL,
+            first_name: name,
+            last_name: lastName,
+            password: password,
+            location: req.body.location
+        };
+        let response = await this.db.GetPrivateUserInfo(email);
+        if (response) {
+            values[password] = response.password;
+        }
+        req.body = values;
+        return !Boolean(response)
+    }
+}
+
+module.exports = Firebase
