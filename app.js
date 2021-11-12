f945dcdab20816af957b9677bb893b9727b089af
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-11-11 23:02:21 -0300

    Allow all entry points into the app

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //Heroku purposes
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
