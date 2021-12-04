e223267eaa5537df21b4decb45c9b838cb9f5dae
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-12-04 02:14:18 -0300

    Finish datadog integration to continue testing with heroku

---
+++ routes/Logger.js
@@ -0,0 +1,20 @@
+const formats = require('dd-trace/ext/formats');
+
+class Logger {
+    constructor(t) {
+        this.tracer = t;
+    }
+    log(level, message) {
+        const span = this.tracer.scope().active();
+        const time = new Date().toISOString();
+        const record = { time, level, message };
+
+        if (span) {
+            this.tracer.inject(span.context(), formats.LOG, record);
+        }
+
+        console.log(JSON.stringify(record));
+    }
+}
+
+module.exports = Logger;
