f64e6e50e6f811293ebd85036cdacb79206ad290
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-12-18 02:45:28 -0300

    add method to send notifications

---
+++ external/courses.js
@@ -0,0 +1,28 @@
+/*global process*/
+const fetch = require('cross-fetch');
+class Courses {
+    constructor() {
+        this.host = process.env.COURSES_API;
+    }
+
+    async sendNotification(info, userId) {
+        let data = {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: info.title,
+                body: info.body,
+                user_id: userId
+            }),
+            method: 'POST'
+        };
+        let res = await fetch(`${this.host}/notification`, data);
+        if (res.status > 299 || !res.ok) {
+            let e = new Error(res.message);
+            e.status = res.statusCode;
+            throw e;
+        }
+        return res.json();
+    }
+}
